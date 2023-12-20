49ef86b46927b7de3e5674e57ff02da824744376
scottd988 <>
2023-12-20 01:56:38 -0500

    added get routes

--- Develop/Develop/public/assets/js/server.js
+++ Develop/Develop/public/assets/js/server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const fs = require("fs");
-const app = express;
-const PORT = process.env.PORT || 3001;
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3002;
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 
 // GET route for landing page
@@ -12,7 +13,26 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-// GET route for notes page
+// GET route to the notes page
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
+
+
+// GET route for retrieving notes
+app.get('api/notes', (req, res) => {
+    fs.readFile('db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(JSON.parse(data));
+        }
+    });
+});
+
+// POST route for adding notes
+
+
+app.listen(PORT, () => 
+    console.log(`App listening on port ${PORT}!`)
+);
